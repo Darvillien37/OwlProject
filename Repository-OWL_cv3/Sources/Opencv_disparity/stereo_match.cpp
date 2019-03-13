90325a04dd455d59a830f21317815ca90c9c4c16
Kaveri30 <48246050+Kaveri30@users.noreply.github.com>
2019-03-13 12:53:14 +0000

    Refactored for readability and formatting
    Also removed comand line parameters option

--- Repository-OWL_cv3/Sources/Opencv_disparity/stereo_match.cpp
+++ Repository-OWL_cv3/Sources/Opencv_disparity/stereo_match.cpp
@@ -18,7 +18,6 @@
 #include "opencv2/core/utility.hpp"
 
 #include <stdio.h>
-#include <winsock2.h>
 
 using namespace cv;
 using namespace std;
@@ -68,14 +67,6 @@ void ConnectAndSend() {
 #endif
 }
 
-static void print_help()
-{
-    printf("\nDemo stereo matching converting L and R images into disparity and point clouds\n");
-    printf("\nUsage: stereo_match <left_image> <right_image> [--algorithm=bm|sgbm|hh|sgbm3way] [--blocksize=<block_size>]\n"
-           "[--max-disparity=<max_disparity>] [--scale=scale_factor>] [-i=<intrinsic_filename>] [-e=<extrinsic_filename>]\n"
-           "[--no-display] [-o=<disparity_image>] [-p=<point_cloud_file>]\n");
-}
-
 static void saveXYZ(const char* filename, const Mat& mat)
 {
     const double max_z = 1.0e4;
@@ -108,7 +99,7 @@ void CallBackFunc(int event, int x, int y, int flags, void* userdata) {
             }
         }
         //cout << "Mouse clicked at: " << x << ", " << y << endl;
-      //Check for right click
+        //Check for right click
     } else if (event == EVENT_RBUTTONDOWN) {
         //Flip the live targeting boolean.
         liveTargeting = !liveTargeting;
@@ -125,127 +116,45 @@ void CallBackFunc(int event, int x, int y, int flags, void* userdata) {
 int main(int argc, char** argv)
 {
     ConnectAndSend();
-    std::string Left_filename = "";
-    std::string Right_filename = "";
+
     std::string intrinsic_filename = "../../Data/intrinsics.xml";
     std::string extrinsic_filename = "../../Data/extrinsics.xml";
-    std::string disparity_filename = "Disparity.jpg";
-    std::string point_cloud_filename = "PointCloud";
 
     enum { STEREO_BM=0, STEREO_SGBM=1, STEREO_HH=2, STEREO_VAR=3, STEREO_3WAY=4 };
     int alg = STEREO_SGBM; //PFC always do SGBM - colour
-    int SADWindowSize, numberOfDisparities;
-    bool no_display;
-    float scale;
-
+    //N-disparities and block size referenced in the lecture.
+    int numberOfDisparities = 256  ; //256 is default.
+    int SADWindowSize = 3; //3 is default.
+    bool no_display = false;
+    float scale = 1.0;
+    int color_mode = alg == STEREO_BM ? 0 : -1;
     int colourSum = 0;
 
     Ptr<StereoBM> bm = StereoBM::create(16,9);
     Ptr<StereoSGBM> sgbm = StereoSGBM::create(0,16,3);
-    cv::CommandLineParser parser(argc, argv,
-                                 "{@arg1||}{@arg2||}{help h||}{algorithm||}{max-disparity|256|}{blocksize|3|}{no-display||}{scale|1|}{i||}{e||}{o||}{p||}");
-    if(parser.has("help"))
-    {
-        print_help();
-        return 0;
-    }
-    //PFC Left_filename = parser.get<std::string>(0);
-    //PFC Right_filename = parser.get<std::string>(1);
-    if (parser.has("algorithm"))
-    {
-        std::string _alg = parser.get<std::string>("algorithm");
-        alg = _alg == "bm" ? STEREO_BM :
-         _alg == "sgbm" ? STEREO_SGBM :
-         _alg == "hh" ? STEREO_HH :
-         _alg == "var" ? STEREO_VAR :
-         _alg == "sgbm3way" ? STEREO_3WAY : -1;
-    }
-    //N-disparities referenced in the lecture.
-    //numberOfDisparities = parser.get<int>("max-disparity"); // = 256.
-    numberOfDisparities = 256; //256 is default.
-    //SADWindowSize = parser.get<int>("blocksize"); // = 3.
-    SADWindowSize = 3; //3 is default.
-    scale = parser.get<float>("scale");
-    no_display = parser.has("no-display");
-    if( parser.has("i") )
-        intrinsic_filename = parser.get<std::string>("i");
-    if( parser.has("e") )
-        extrinsic_filename = parser.get<std::string>("e");
-    if( parser.has("o") )
-        disparity_filename = parser.get<std::string>("o");
-    if( parser.has("p") )
-        point_cloud_filename = parser.get<std::string>("p");
-    if (!parser.check())
-    {
-        parser.printErrors();
-        return 1;
-    }
-    if( alg < 0 )
-    {
-        printf("Command-line parameter error: Unknown stereo algorithm\n\n");
-        print_help();
-        return -1;
-    }
+
     if ( numberOfDisparities < 1 || numberOfDisparities % 16 != 0 )
     {
         printf("Command-line parameter error: The max disparity (--maxdisparity=<...>) must be a positive integer divisible by 16\n");
-        print_help();
         return -1;
     }
     if (scale < 0)
     {
-        printf("Command-line parameter error: The scale factor (--scale=<...>) must be a positive floating-point number\n");
+        printf("Error: The scale factor variable must be a positive floating-point number\n");
         return -1;
     }
     if (SADWindowSize < 1 || SADWindowSize % 2 != 1)
     {
-        printf("Command-line parameter error: The block size (--blocksize=<...>) must be a positive odd number\n");
+        printf("Error: The block size variable must be a positive odd number\n");
         return -1;
     }
-/*PFC    if( Left_filename.empty() || Right_filename.empty() )
-    {
-        printf("Command-line parameter error: both left and right images must be specified\n");
-        return -1;
-    }
-    */
+
     if( (!intrinsic_filename.empty()) ^ (!extrinsic_filename.empty()) )
     {
-        printf("Command-line parameter error: either both intrinsic and extrinsic parameters must be specified, or none of them (when the stereo pair is already rectified)\n");
+        printf("Error: either both intrinsic and extrinsic parameters must be specified, or none of them (when the stereo pair is already rectified)\n");
         return -1;
     }
 
-    if( extrinsic_filename.empty() && !point_cloud_filename.empty() )
-    {
-        printf("Command-line parameter error: extrinsic and intrinsic parameters must be specified to compute the point cloud\n");
-        return -1;
-    }
-
-    int color_mode = alg == STEREO_BM ? 0 : -1;
-/* PFC
-    Mat Left = imread(Left_filename, color_mode);
-    Mat Right = imread(Right_filename, color_mode);
-
-    if (Left.empty())
-    {
-        printf("Command-line parameter error: could not load the first input image file\n");
-        return -1;
-    }
-    if (Right.empty())
-    {
-        printf("Command-line parameter error: could not load the second input image file\n");
-        return -1;
-    }
-
-    if (scale != 1.f)
-    {
-        Mat temp1, temp2;
-        int method = scale < 1 ? INTER_AREA : INTER_CUBIC;
-        resize(Left, temp1, Size(), scale, scale, method);
-        Left = temp1;
-        resize(Right, temp2, Size(), scale, scale, method);
-        Right = temp2;
-    }
-*/
 
     Rect roi1, roi2;
     Mat Q;
@@ -310,15 +219,9 @@ int main(int argc, char** argv)
                 cout  << "Could not open the input video: " << source << endl;
                 //         break;
             }
-            // Mat FrameFlpd; cv::flip(Frame,FrameFlpd,1); // Note that Left/Right are reversed now
-            //Mat Gray; cv::cvtColor(Frame, Gray, cv::COLOR_BGR2GRAY);
             // Split into LEFT and RIGHT images from the stereo pair sent as one MJPEG iamge
             Right= Frame( Rect(0, 0, img_size.width, img_size.height)); // using a rectangle
             Left= Frame( Rect(img_size.width, 0, img_size.width, img_size.height)); // using a rectangle
-            //DEBUG imshow("Left",Left);imshow("Right", Right);
-            //waitKey(30); // display the images
-
-
 
             Mat Leftr, Rightr;
             remap(Left, Leftr, map11, map12, INTER_LINEAR);
@@ -327,13 +230,10 @@ int main(int argc, char** argv)
             Left = Leftr;
             Right = Rightr;
 
-
-            numberOfDisparities = numberOfDisparities > 0 ? numberOfDisparities : ((img_size.width/8) + 15) & -16;
-
             bm->setROI1(roi1);
             bm->setROI2(roi2);
             bm->setPreFilterCap(31);
-            bm->setBlockSize(SADWindowSize > 0 ? SADWindowSize : 9);
+            bm->setBlockSize(SADWindowSize);
             bm->setMinDisparity(0);
             bm->setNumDisparities(numberOfDisparities);
             bm->setTextureThreshold(10);
@@ -348,8 +248,8 @@ int main(int argc, char** argv)
 
             int cn = Left.channels();
 
-            sgbm->setP1(8*cn*sgbmWinSize*sgbmWinSize);
-            sgbm->setP2(32*cn*sgbmWinSize*sgbmWinSize);
+            sgbm->setP1(8 * cn * sgbmWinSize * sgbmWinSize);
+            sgbm->setP2(32 * cn * sgbmWinSize * sgbmWinSize);
             sgbm->setMinDisparity(0);
             sgbm->setNumDisparities(numberOfDisparities);
             sgbm->setUniquenessRatio(10);
@@ -363,9 +263,6 @@ int main(int argc, char** argv)
             else if(alg==STEREO_3WAY)
                 sgbm->setMode(StereoSGBM::MODE_SGBM_3WAY);
 
-            //Mat Leftp, Rightp, dispp;
-            //copyMakeBorder(Left, Leftp, 0, 0, numberOfDisparities, 0, IPL_BORDER_REPLICATE);
-            //copyMakeBorder(Right, Rightp, 0, 0, numberOfDisparities, 0, IPL_BORDER_REPLICATE);
 
             int64 t = getTickCount();
             if( alg == STEREO_BM )
@@ -373,86 +270,71 @@ int main(int argc, char** argv)
             else if( alg == STEREO_SGBM || alg == STEREO_HH || alg == STEREO_3WAY )
                 sgbm->compute(Left, Right, disp);
             t = getTickCount() - t;
-            printf("Time elapsed: %fms\n", t*1000/getTickFrequency());
+            //calculates the time elapsed for calculation
+            printf("Time elapsed: %fms\n", t * 1000 / getTickFrequency());
 
-            //disp = dispp.colRange(numberOfDisparities, Leftp.cols);
+
             if( alg != STEREO_VAR ) {
-                disp.convertTo(disp8, CV_8U, 255/(numberOfDisparities*16.));
+                disp.convertTo(disp8, CV_8U, 255/(numberOfDisparities * 16.0));
             } else {
                 disp.convertTo(disp8, CV_8U);
             }
 
-            if( true )
-            {   //Flipping all of the frames on the x axis before displaying.
-                //namedWindow("left", 1);
-                flip(Left,Left,-1);
-                imshow("left", Left);
-                //namedWindow("right", 1);
-                flip(Right,Right,-1);
-                imshow("right", Right);
-                //namedWindow("disparity", 0);
+            //----------Displaying windows stuff--------------
 
-                //Check for mouse clicks on disparity window before any calculations.
-                setMouseCallback("disparity", CallBackFunc, NULL);
+            //Flipping all of the frames on the x axis before displaying.
+            flip(Left, Left, -1);
+            flip(Right, Right, -1);
+            imshow("left", Left);
+            imshow("right", Right);
 
-                //Calculations for distance begin here.
-                //Grab target here.
-                //targetArray = disp8(target);
-                disp8(target).copyTo(targetArray);
-                //Loop through entire target.
-                for (int i = 0; i < targetSize; i++) {
-                    for (int j = 0; j < targetSize; j++) {
-                        //Get the pixel at i, j
-                        targetColour = targetArray.at<uchar>(Point(i, j));
-                        //Add the greyscale value of said pixel to colour sum.
-                        colourSum += targetColour[0];
-                    }
+            //Check for mouse clicks on disparity window before any calculations.
+            setMouseCallback("disparity", CallBackFunc, NULL);
+
+            //Calculations for distance begin here.
+            //Grab target here.
+            disp8(target).copyTo(targetArray);
+
+            //Reset colour sum before use.
+            colourSum = 0;
+            //Loop through every pixel on target.
+            for (int i = 0; i < targetSize; i++) {
+                for (int j = 0; j < targetSize; j++) {
+                    //Get the pixel at i, j
+                    targetColour = targetArray.at<uchar>(Point(i, j));
+                    //Add the greyscale value of said pixel to colour sum.
+                    colourSum += targetColour[0];
                 }
-
-                //Need to divide sum by amount of values to get average.
-                colourSum = colourSum / pow(targetSize, 2);
-
-                //Put the value into distanceString for printing to the window.
-                distanceString = to_string((int)colourSum) + "mm";
-
-                //Reset colour sum after use.
-                colourSum = 0;
-
-                //If we are live targeting, flip the target and display it.
-                if (liveTargeting) {
-                    flip(targetArray, targetArray, -1);
-                    imshow("liveTarget", targetArray);
-                }
-
-                //disp8 needs to be flipped before it can be shown.
-                flip(disp8,disp8,-1);
-
-                //Show target on disparity frame.
-                rectangle(disp8, displayTarget, Scalar::all(255), 1, 8, 0);
-                //Write on disparity window.
-                putText(disp8, "Distance:", cvPoint(450, 50), FONT_HERSHEY_DUPLEX, 1, Scalar::all(255), 0, 0, false);
-                putText(disp8, distanceString, cvPoint(475, 80), FONT_HERSHEY_DUPLEX, 1, Scalar::all(255), 0, 0, false);
-                imshow("disparity", disp8);
-                //printf("press any key to continue...");
-                //fflush(stdout);
-                char key=waitKey(30);
-                if (key=='q') break;
-                //printf("\n");
             }
+            //Need to divide sum by amount of values to get average.
+            colourSum = colourSum / pow(targetSize, 2);
+
+            //Put the value into distanceString for printing to the window.
+            distanceString = to_string((int)colourSum) + "mm";
+
+            //If we are live targeting, flip the target and display it.
+            if (liveTargeting) {
+                flip(targetArray, targetArray, -1);
+                imshow("liveTarget", targetArray);
+            }
+
+            //disp8 needs to be flipped before it can be shown.
+            flip(disp8,disp8,-1);
+
+            //Show target on disparity frame.
+            rectangle(disp8, displayTarget, Scalar::all(255), 1, 8, 0);
+            //Write on disparity window.
+            putText(disp8, "Distance:", cvPoint(450, 50), FONT_HERSHEY_DUPLEX, 1, Scalar::all(255), 0, 0, false);
+            putText(disp8, distanceString, cvPoint(475, 80), FONT_HERSHEY_DUPLEX, 1, Scalar::all(255), 0, 0, false);
+            imshow("disparity", disp8);
+
+            //Exit the disparity loop on a key press of 'q'
+            char key = waitKey(30);
+            if (key=='q') {
+                break;
+            }
+
         } // end video loop
-
-        if(!disparity_filename.empty())
-            imwrite(disparity_filename, disp8);
-
-        if(!point_cloud_filename.empty())
-        {
-            printf("storing the point cloud...");
-            fflush(stdout);
-            Mat xyz;
-            reprojectImageTo3D(disp, xyz, Q, true);
-            saveXYZ(point_cloud_filename.c_str(), xyz);
-            printf("\n");
-        }
     } // end got intrinsics IF
 #ifdef _WIN32
     closesocket(u_sock);
